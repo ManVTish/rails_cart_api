96ceb6760d1cba971d191f52240b14f116bfa3ed
Manish <manishroy0112@gmail.com>
2022-08-15 16:08:32 +0530

    relation of many to many between products & cart is added though lineitem

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ApplicationRecord
     validates :title, :description, :image_url, presence: true
     validates :title, uniqueness: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+    has_many :line_items
+    before_destroy :not_referenced_by_any_line_item
+
+    private
+    def not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Item is present')
+        throw :abort
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_080011) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_100735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -46,4 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_080011) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
